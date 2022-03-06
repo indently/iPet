84d1ff11b4ac90dab63ec61d38d7ccb9bb94b6cf
Federico <codepalace@fastmail.se>
2022-03-06 11:34:16 +0100

    Initial Commit

---
+++ iPet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPet
+//
+//  Created by Federico on 06/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPet/iPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPetApp.swift
+//  iPet
+//
+//  Created by Federico on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
